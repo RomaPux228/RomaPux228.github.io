1d20f3332b7318e1d1202ce64661a150f36a527d
RomaPux228 <50229125+RomaPux228@users.noreply.github.com>
2019-07-26 13:25:16 +0300

    vk.com/vk_moders_dev

---
+++ precache-manifest.c4279db618eafaab935375becf68af24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "0354448eaecfc7f891f6",
+    "url": "/static/js/main.0354448e.chunk.js"
+  },
+  {
+    "revision": "ac3f15c4898e85c5b7a8",
+    "url": "/static/js/2.ac3f15c4.chunk.js"
+  },
+  {
+    "revision": "0354448eaecfc7f891f6",
+    "url": "/static/css/main.e854548e.chunk.css"
+  },
+  {
+    "revision": "ac3f15c4898e85c5b7a8",
+    "url": "/static/css/2.64406258.chunk.css"
+  },
+  {
+    "revision": "a499fe293ad9e97d8b0cddf1c6dfc69d",
+    "url": "/index.html"
+  }
+];
